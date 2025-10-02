37835cb75ef3d7d0f17c3e2b354414ba5d98b376
Brayan Gonzalez <150887917+brayang2014@users.noreply.github.com>
2025-10-02 14:44:10 -0500

    set configurations to ElevenLabs agent

--- voice_assistant.py
+++ voice_assistant.py
@@ -15,3 +15,30 @@ user_name = "Brayan"
 schedule = "Class from 10-11:30; 12-8: study o'clock"
 prompt = f"You are a helpful assistant. Your interlocutor has the following schedule: {schedule}"
 first_message = f"Hello, {user_name}, how can I help you today?"
+
+conversation_override = {
+    "agent": {
+        "prompt":{
+            "prompt":prompt
+            },
+
+        "first_mesage": first_message
+    },   
+}
+
+config = ConversationConfig(
+    conversation_config_override = conversation_override,
+    extra_body={},
+    dynamic_variables={},
+)
+
+client = ElevenLabs(api_key=API_KEY)
+
+conversation = Conversation(
+    client,
+    AGENT_ID,
+    config=config,
+    requires_auth=True,
+    audio_interface=DefaultAudioInterface(),
+)
+
